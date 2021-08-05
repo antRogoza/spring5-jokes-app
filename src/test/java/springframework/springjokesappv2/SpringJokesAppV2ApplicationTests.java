f1f5222a34dc9f5ec113d102341c449dd01142dc
arohoza <anton.rohoza@gmail.com>
2021-08-05 09:41:07 +0300

    init jokes app

---
+++ src/test/java/springframework/springjokesappv2/SpringJokesAppV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package springframework.springjokesappv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesAppV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
